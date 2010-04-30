a4ebf2723510abe32d5d15e6fa31247cce4c5e73
Brian West <bnwest@rice.edu>
2010-04-30 18:36:29 +0000

    only prepend 'derive copy of' once. fixes ticket:10269.

--- Products/RhaptosContent/browser/reuse_edit.py
+++ Products/RhaptosContent/browser/reuse_edit.py
@@ -254,10 +254,6 @@ class ReuseEditView(BrowserView):
             obj.setState('published')
             obj.checkout(content.objectId)    
 
-            # Adjust the title
-            title = _("Derived copy of") + " " + obj.Title()
-            obj.setTitle(title)
-
             # Do the fork
             state = ControllerState()
             self.request.set('controller_state', state)
