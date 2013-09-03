c30cf59127bf702bc9b5c1271e131e2adadff4d7
Ross Reedstrom <reedstrm@gmail.com>
2013-09-03 15:32:30 -0500

    initial licensing work

--- Products/RhaptosContent/skins/rhaptos_content/getDefaultLicense.py
+++ Products/RhaptosContent/skins/rhaptos_content/getDefaultLicense.py
@@ -4,8 +4,10 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=
+##parameters=license_code='by'
 ##title= Handy temporary place to put the default license URL until we have a tool
 
-return 'http://creativecommons.org/licenses/by/3.0/'
-
+if 'nc' in license_code:
+    return 'http://creativecommons.org/licenses/by-nc-sa/3.0/'
+else:
+    return 'http://creativecommons.org/licenses/by/3.0/'
